28ff7e52985661f3b26d388264d8c449c9354bef
Gabriel Cassimiro <gabriel.cassimiro@happycodeschool.com>
2020-04-12 02:58:33 -0300

    Camera in development.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float movementTime;
+
+    [SerializeField] private Vector3 newPosition;
+
+    private void Start() {
+        newPosition = transform.position;
+    }
+
+    private void Update() {
+        MovementInput();
+    }
+
+    private void MovementInput() {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            newPosition += (transform.forward * speed);
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            newPosition += (transform.forward * -speed);
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            newPosition += (transform.right * speed);
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            newPosition += (transform.right * -speed);
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+    }
+}
