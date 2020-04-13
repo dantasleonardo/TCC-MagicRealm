db61df51f391e6fa85cf3c334aa84eeb7f911b37
Gabriel Cassimiro <gabriel.cassimiro@happycodeschool.com>
2020-04-13 17:26:40 -0300

    Selection Unit Base Completed

--- Assets/Scripts/Player/UnitSelection.cs
+++ Assets/Scripts/Player/UnitSelection.cs
@@ -1,8 +1,8 @@
-﻿
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Unity.Mathematics;
 using UnityEditor.Rendering;
 using UnityEditorInternal.Profiling.Memory.Experimental.FileFormat;
 using UnityEngine;
@@ -13,10 +13,12 @@ public class UnitSelection : MonoBehaviour
     [SerializeField] private LayerMask unitLayerMask;
 
     [SerializeField] private List<Unit> selectedUnits = new List<Unit>();
+    private List<Unit> forDesactiveUnits = new List<Unit>();
     [SerializeField] private Vector2 startPosition;
 
-    [Header("Components")] 
-    [SerializeField] private Camera mainCamera;
+    [Header("Components")] [SerializeField]
+    private Camera mainCamera;
+
     [SerializeField] private UnitController unitController;
 
     private void Awake() {
@@ -26,7 +28,14 @@ public class UnitSelection : MonoBehaviour
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            selectedUnits = new List<Unit>();
+            if (selectedUnits.Count > 0) {
+                DesactiveSelectionUnit();
+                selectedUnits = new List<Unit>();
+            }
+            else {
+                selectedUnits = new List<Unit>();
+            }
+
             startPosition = Input.mousePosition;
         }
 
@@ -35,31 +44,17 @@ public class UnitSelection : MonoBehaviour
         }
 
         if (Input.GetMouseButtonUp(0)) {
-            if(startPosition.Equals(Input.mousePosition))
-                TrySelect(Input.mousePosition);
-            else
-                ReleaseSelectionBox();
+            ReleaseSelectionBox();
         }
     }
 
-    private void TrySelect(Vector2 screenPosition) {
-        Ray ray = mainCamera.ScreenPointToRay(screenPosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, unitLayerMask)) {
-            Unit unit = hit.collider.GetComponent<Unit>();
-            Debug.Log("Object Selected");
-            selectedUnits.Add(unit);
-        }
-    }
-    
     private void UpdateSelectionBox(Vector2 currentMousePosition) {
-        if(!selectionBox.gameObject.activeInHierarchy)
+        if (!selectionBox.gameObject.activeInHierarchy)
             selectionBox.gameObject.SetActive(true);
 
         float width = currentMousePosition.x - startPosition.x;
         float height = currentMousePosition.y - startPosition.y;
-        
+
         selectionBox.sizeDelta = new Vector2(Math.Abs(width), Math.Abs(height));
         selectionBox.anchoredPosition = startPosition + new Vector2(width / 2, height / 2);
     }
@@ -69,15 +64,34 @@ public class UnitSelection : MonoBehaviour
 
         Vector2 min = selectionBox.anchoredPosition - (selectionBox.sizeDelta / 2);
         Vector2 max = selectionBox.anchoredPosition + (selectionBox.sizeDelta / 2);
+
+        float selectionAreaMinSize = 10f;
+        float selectionAreaSize = math.distance(min, max);
         
+        if (selectionAreaSize < selectionAreaMinSize) {
+            min = Input.mousePosition + new Vector3(-50, -50, 0);
+            max = Input.mousePosition + new Vector3(50, 50, 0);
+            Debug.Log("entrou no if");
+        }
+        
+
         unitController.units.ForEach(unit => {
             Vector3 screenPosition = mainCamera.WorldToScreenPoint(unit.transform.position);
-
+            
             if (screenPosition.x >= min.x && screenPosition.y >= min.y && screenPosition.x <= max.x &&
                 screenPosition.y <= max.y) {
+                unit.SelectionCircleIsActive(true);
                 selectedUnits.Add(unit);
-                Debug.Log("Object Selected");
+                forDesactiveUnits.Add(unit);
             }
         });
+    }
+
+    private void DesactiveSelectionUnit() {
+        foreach (var unit in forDesactiveUnits) {
+            unit.SelectionCircleIsActive(false);
+        }
+
+        forDesactiveUnits = new List<Unit>();
     }
 }
