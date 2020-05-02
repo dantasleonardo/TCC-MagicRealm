de09bcf587fae8b633b15a032164fc23c81d870f
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-05-02 19:40:06 -0300

    Press start

---
+++ Assets/Scripts/Menu/LoadMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+}
