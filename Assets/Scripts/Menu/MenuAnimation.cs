b1ff06c542a2cc030268164dab43e9f4dd077da7
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-05-07 17:51:27 -0300

    New fonts

--- Assets/Scripts/Menu/MenuAnimation.cs
+++ Assets/Scripts/Menu/MenuAnimation.cs
@@ -29,7 +29,7 @@ public class MenuAnimation : MonoBehaviour
     {
         menu.SetActive(true);
         //Move as opções do menu, fazendo assim eles aparecerem de fora da tela
-        LeanTween.moveX(menu, 700.0f, 1.0f);
+        LeanTween.moveX(menu, 750.0f, 1.0f);
     }
 
     //Chamado quando inicia a transição da camera
