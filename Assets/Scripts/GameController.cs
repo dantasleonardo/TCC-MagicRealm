a1dd4b20c0b16854bc3f2df6decf8f6dbfff703d
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2020-05-12 20:53:06 -0300

    Remove files.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
