ec69c74bec55b024664ff089c284c1845a862948
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-05-21 18:33:45 -0300

    Menu and panel att

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Pause : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
     public bool gameIsPaused;
     public GameObject panelPause;
