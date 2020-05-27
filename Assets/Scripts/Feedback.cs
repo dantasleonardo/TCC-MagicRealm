16b08f0363c2f4b9551c67f6fb347b53d7db0bb8
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2020-05-26 21:55:36 -0300

    Alpha version

--- Assets/Scripts/Feedback.cs
+++ Assets/Scripts/Feedback.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Feedback : MonoBehaviour
 {
@@ -8,5 +9,9 @@ public class Feedback : MonoBehaviour
 
     public void linkFeedback() {
         Application.OpenURL(url);
+    }
+
+    public void BackToMenu() {
+        SceneManager.LoadScene("Menu");
     }
 }
