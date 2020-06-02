fdb0c79c3d9a9bb038a8e4c6c63b9258773e18cd
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2020-06-01 22:18:08 -0300

    Updated Scene and Start AI with Panda BT

--- Assets/Scripts/AI/BotAi.cs
+++ Assets/Scripts/AI/BotAi.cs
@@ -6,7 +6,7 @@ public class BotAi : MonoBehaviour
 {
     public IEnemy enemy;
     public BaseAI baseAi;
-    public Animator animator;
+    //public Animator animator;
 
     [SerializeField] private float speed = 1.5f;
 
@@ -16,6 +16,9 @@ public class BotAi : MonoBehaviour
     [SerializeField] private bool flee;
     [SerializeField] private bool attacking;
     public Transform target;
+
+    public float timeFirerate = 2.0f;
+    public float timeCount = 0.0f;
 
     [Header("Decisions")]
     public float distanceSeek;
@@ -32,7 +35,7 @@ public class BotAi : MonoBehaviour
 
     private void Start() {
         baseAi = GetComponent<BaseAI>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         baseAi.agent.speed = speed;
         enemy = GetComponent<IEnemy>();
     }
@@ -40,14 +43,14 @@ public class BotAi : MonoBehaviour
     private void Update() {
         currentSpeed = Mathf.Lerp(speed, (transform.position - lastPosition).magnitude / Time.deltaTime, 0.75f);
         lastPosition = transform.position;
-        animator.SetFloat("Speed", currentSpeed);
+        //animator.SetFloat("Speed", currentSpeed);
     }
 
     public void UpdateAnimation(bool seek, bool waypoint, bool flee, bool attacking) {
-        animator.SetBool("Seek", seek);
-        animator.SetBool("Waypoint", waypoint);
-        animator.SetBool("Flee", flee);
-        animator.SetBool("Attacking", attacking);
+        //animator.SetBool("Seek", seek);
+        //animator.SetBool("Waypoint", waypoint);
+        //animator.SetBool("Flee", flee);
+        //animator.SetBool("Attacking", attacking);
     }
 
     public Quaternion LookTarget() {
