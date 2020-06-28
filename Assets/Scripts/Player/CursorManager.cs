f4252dfc7ec2b1b6617062b4f90dae2f18eab59b
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2020-06-28 13:05:29 -0300

    Cursor Personalizado.

---
+++ Assets/Scripts/Player/CursorManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D attackCursor;
+    [SerializeField] private Texture2D collectCursor;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        SetCursor();
+    }
+
+    private void SetCursor()
+    {
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if(hit.collider.CompareTag("Resources"))
+                Cursor.SetCursor(collectCursor,new Vector2(10.0f,10.0f), CursorMode.Auto);
+            else if (hit.collider.CompareTag("Mages"))
+            {
+                Cursor.SetCursor(attackCursor,new Vector2(10.0f,10.0f), CursorMode.Auto);
+            }
+        }
+    }
+}
